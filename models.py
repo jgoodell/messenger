5cbc930d1e03f4f7604d7a1a2bfe23cff2b7b462
Jason Goodell <jason.goodell@harrison.edu>
2014-08-11 12:38:10 -0400

    Added tests and a model. All test pass.

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=200)
+    time_stamp = models.DateTimeField('time stamp')
+
+    def __unicode__(self):
+        return self.text
